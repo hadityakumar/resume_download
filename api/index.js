08ee102e85af2c1c7576c98abc2c98c8f6dfab22
hadityakumar <hadityakumar@gmail.com>
2024-07-08 14:17:22 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const cors = require('cors');
+const express = require('express');
 const path = require('path');
+const serverless = require('serverless-http');
 
-app.use(cors());
+const app = express();
 
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, '../aditya_resume.pdf');
     res.sendFile(filePath);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports.handler = serverless(app);
